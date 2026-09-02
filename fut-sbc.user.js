9494cd7c5c772efd7b48662498faa9901231d1ac
Miguel <mljpa@github.com>
2026-09-02 17:19:22 -0400

    build 0.1.0.1788383961

--- fut-sbc.user.js
+++ fut-sbc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FUT SBC Solver v2
 // @namespace    https://github.com/mljpa/fut-sbc-solver-v2
-// @version      0.1.0.1788383716
+// @version      0.1.0.1788383961
 // @description  Userscript to solve EA SPORTS FC 26 SBCs with your own club
 // @match        https://www.ea.com/*/ea-sports-fc/ultimate-team/web-app*
 // @match        https://www.ea.com/ea-sports-fc/ultimate-team/web-app*
@@ -3424,7 +3424,8 @@ ${text}`);
       submitChallenge,
       dismissPostSubmit,
       reenterChallenge,
-      repeatability
+      repeatability,
+      resetPoolCache
     };
     let handle = null;
     let mountedFor = -1;
